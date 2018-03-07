d91448d461c442602f632b5ada9d7341361ca4c3
Carlos Villicaña Alva <villicana.ca@gmail.com>
2018-03-07 14:53:42 -0600

    add auth route

--- routes.js
+++ routes.js
@@ -1,12 +1,21 @@
-
 var TodoController            = require('./controllers/todos'),
+    AuthController            = require('./controllers/auth'),
     express                   = require('express'),
+    passport                  = require('passport'),
     multer                    = require('multer');
+
+var requireAuth = passport.authenticate('jwt', {session: false}),
+    requireLogin = passport.authenticate('local', {session: false});
 
 module.exports = function(app){
 
     var apiRoutes     = express.Router(),
+        authRoutes    = express.Router(),
         todoRoutes    = express.Router();
+
+    // Auth Routes
+    apiRoutes.use('/auth', authRoutes);
+    authRoutes.post('/register', AuthController.register);
 
     // Todo Routes
     apiRoutes.use('/todos', todoRoutes);
